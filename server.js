d2d5ea292aaf5bea121813bf384faef6d24ad13b
Greg Hawthorne <ghawthorne@me.com>
2020-01-28 01:23:28 -0800

    add heroku post build

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const path = require('path');
 const Mailchimp = require('mailchimp-api-v3');
-require('dotenv').config({ path: _dirname + "/variables.env"});
+require('dotenv').config({ path: __dirname + "/variables.env"});
 
-const mc_api_key = process.env.MAILCHIMP_API_KEY
+const mc_api_key = process.env.MAILCHIMP_API_KEY;
+const list_id = process.env.LIST_ID;
+
+//serve static files from react app
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 const app = express();
-
 const mailchimp = new Mailchimp(mc_api_key);
+
 
 //API endpoint
 app.get("/api/memberAdd" , (req, res) => {
@@ -16,4 +20,19 @@ app.get("/api/memberAdd" , (req, res) => {
         email_address: req.query.email,
         status: "subscribed"
     })
+    .then(result => {
+        res.send(result);
+    })
+    .catch(err => {
+        res.send(err);
+    })
+});
+
+app.get('*' , (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+
 })
+const port = process.env.PORT || 9001;
+app.listen(port);
+
+console.log(`Express is listening on port ${ port }`)
